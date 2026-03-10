d8227fd65c8762687042935b28d0ff474f260197
TheVaro93 <adrien.virorello@gmail.com>
2026-03-10 11:32:02 +0000

    feat: ajouter la zone de dépôt pour le téléchargement de fichiers et mettre à jour les images de fond

--- upload.js
+++ upload.js
@@ -1,9 +1,9 @@
 const images = [
 
-"https://picsum.photos/1920/1080?1",
-"https://picsum.photos/1920/1080?2",
-"https://picsum.photos/1920/1080?3",
-"https://picsum.photos/1920/1080?4"
+"https://www.mmv.fr/images/cms/paysage-montagne-ete/paysage-montagne-mont-blanc.jpg?frz-v=647",
+"https://www.okvoyage.com/wp-content/uploads/2022/11/paysages-de-montagne.jpg",
+"https://www.france-montagnes.com/wp-content/uploads/2024/12/panorama-lac-dallos-montagne-c-florianscala-1280x645-1734687992.jpg",
+"https://img-4.linternaute.com/Eii63OQvZBqK5I5uoP9oWdyPecQ=/1240x/smart/685fdd5cfcf444e08d3ff22cde5d1581/ccmcms-linternaute/2377659.jpg"
 
 ];
 
@@ -24,3 +24,25 @@ index = 0;
 changeBackground();
 
 setInterval(changeBackground, 120000);
+
+const dropZone = document.getElementById("drop-zone")
+const fileInput = document.getElementById("file-input")
+
+dropZone.addEventListener("dragover", e=>{
+    e.preventDefault()
+    dropZone.classList.add("hover")
+})
+
+dropZone.addEventListener("dragleave", ()=>{
+    dropZone.classList.remove("hover")
+})
+
+dropZone.addEventListener("drop", e=>{
+    e.preventDefault()
+    fileInput.files = e.dataTransfer.files
+    dropZone.classList.remove("hover")
+})
+
+function generateID(){
+return Math.random().toString(36).substring(2,8)
+}
